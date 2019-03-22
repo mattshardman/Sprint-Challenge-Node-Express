c8148af6d01fa0c90f0df0258b65f20d6ef4f08c
mattshardman <mattshardman@gmail.com>
2019-03-22 08:59:17 +0000

    add get method and post method for projects

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const routes = express.Router();
 
+const db = require('../data/helpers/projectModel');
+
+routes.get('/api/projects', async (req, res, next) => {
+    try {
+        const projects = await db.get();
+        res.status(200).json(projects);
+    } catch (e) {
+        next({ status: 500, message: 'Could not find projects' });
+    }
+})
+
+routes.post('/api/projects', async (req, res, next) => {
+    try {
+        const project = await db.insert(req.body);
+        res.status(200).json(project);
+    } catch (e) {
+        next({ status: 500, message: 'Could not create project' });
+    }
+})
+
 module.exports = routes;
