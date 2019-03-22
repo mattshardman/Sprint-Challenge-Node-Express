db4c460e55816373e0b6bda447cdbcc0a8112fad
mattshardman <mattshardman@gmail.com>
2019-03-22 08:22:30 +0000

    add actionRoutes and projectRoutes files for relevant routes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
+const projectRoutes = require('./routes/projectRoutes');
+const actionRoutes = require('./routes/actionRoutes');
+
 app.use(express.json());
 
-
+app.use(projectRoutes);
+app.use(actionRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
