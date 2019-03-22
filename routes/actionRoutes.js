0f97c2d899f14033a57fe934e8f514b078adf516
mattshardman <mattshardman@gmail.com>
2019-03-22 09:07:33 +0000

    add update method for actions

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -1,25 +1,42 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 
-const db = require('../data/helpers/actionModel');
+const db = require("../data/helpers/actionModel");
 
-routes.get('/api/actions', async (req, res, next) => {
-    try {
-        const actions = await db.get();
-        res.status(200).json(actions);
-
-    } catch (e) {
-        next({ status: 500, message: 'Could not get actions' });
-    }
+routes.get("/api/actions", async (req, res, next) => {
+  try {
+    const actions = await db.get();
+    res.status(200).json(actions);
+  } catch (e) {
+    next({ status: 500, message: "Could not get actions", description: e });
+  }
 });
 
-routes.post('/api/actions', async (req, res, next) => {
-    try {
-        const action = await db.insert(req.body);
-        res.status(200).json(action);
-    } catch (e) {
-        next({ status: 500, message: 'Could not create an action' });
-    }
+routes.post("/api/actions", async (req, res, next) => {
+  try {
+    const action = await db.insert(req.body);
+    res.status(200).json(action);
+  } catch (e) {
+    next({
+      status: 500,
+      message: "Could not create an action",
+      description: e
+    });
+  }
+});
+
+routes.put("/api/actions/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const action = await db.update(id, req.body);
+    res.status(200).json(action);
+  } catch (e) {
+    next({
+      status: 500,
+      message: `Could not update record ${id}`,
+      description: e
+    });
+  }
 });
 
 module.exports = routes;
